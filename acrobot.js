378f366ceafa4eca623a7be099ba8741849f598e
Steven Bakhtiari <steven.bakh@gmail.com>
2016-07-22 09:52:35 +0100

    Refactor bot to support deployment as an app

--- acrobot.js
+++ acrobot.js
@@ -1,8 +1,20 @@
 
+const config = { json_file_store: 'acrobot_db' };
+var controller;
+
+if (process.env.TOKEN) {
+    // run the bot as a custom integration
+    controller = require('./custom_integration').init(process.env.TOKEN, config, loadData);
+} else if (process.env.PORT && process.env.CLIENT_ID && process.env.CLIENT_SECRET) {
+    // run the bot as an app
+    controller = require('./app').init(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, loadData);
+} else {
+    console.error('Missing environment variables!');
+    process.exit(1);
+}
+
 var acronyms = {};
-
 var listening = new Set();
-
 var stopWords = [
     "AND",
     "THE",
@@ -11,14 +23,14 @@ var stopWords = [
     "AN"
 ];
 
-const config = { json_file_store: 'acrobot_db' };
-const controller = require('./custom_integration').init(process.env.token, config, loadData);
 const entities = require('entities');
 
 function loadData(bot) {
-    controller.storage.teams.get(bot.team_info.id, (err, data) => {
+    controller.storage.teams.get(bot.team_info.id, (error, data) => {
         if (data !== undefined && data.acronyms !== undefined)
-            acronyms = data.acronyms;
+            acronyms[bot.team_info.id] = data.acronyms;
+        else
+            acronyms[bot.team_info.id] = {};
     });
 
     controller.storage.channels.all((error, channels) => {
@@ -33,6 +45,14 @@ function normaliseAcronym(acronym) {
         .toUpperCase()
 }
 
+function saveTeamAcronyms(teamId, callback) {
+    controller.storage.teams.get(teamId, (error, data) => {
+        data.acronyms = acronyms[teamId];
+        callback = callback || ((error) => console.error(error));
+        controller.storage.teams.save(data, callback);
+    });
+}
+
 const directly = ['direct_message', 'mention', 'direct_mention'];
 
 controller.hears('ping', directly, (bot, message) => bot.reply(message, 'pong'));
@@ -41,17 +61,17 @@ controller.hears([
     /(?:\W)?([A-Za-z0-9\.]+)(?:\W)? (?:means|stands for) (?:\W)?(.+?)(?:\W)?$/
 ], directly, (bot, message) => {
 
+    const teamId = bot.team_info.id;
     const acronym = normaliseAcronym(message.match[1]);
     const expansion = entities.decodeHTML(message.match[2]);
 
-    if (acronyms.hasOwnProperty(acronym)) {
+    if (acronyms[teamId].hasOwnProperty(acronym)) {
         console.log(`Acronym '${acronym}' already defined!`);
-        bot.reply(message, `Sorry! '${acronym}' has already been defined!`);
+        bot.reply(message, `Sorry! '${acronym}' has already been defined! Feel free to update it, though.`);
     } else {
         console.log(`User saved acronym: '${acronym}' for '${expansion}'`);
-        acronyms[acronym] = expansion;
-        controller.storage.teams.save({ id: message.team, acronyms });
-        bot.reply(message, 'Oooh, a new acronym! Thanks!');
+        acronyms[teamId][acronym] = expansion;
+        saveTeamAcronyms(teamId, () => bot.reply(message, 'Oooh, a new acronym! Thanks!'));
     }
 });
 
@@ -59,12 +79,13 @@ controller.hears([
     /(?:remove|delete) (?:\W)?(.+?)(?:\W)?$/
 ], directly, (bot, message) => {
 
+    const teamId = bot.team_info.id;
     const acronym = normaliseAcronym(message.match[1]);
     console.log(`User requested deletion of acronym: '${acronym}'`);
 
-    if (acronyms.hasOwnProperty(acronym)) {
-        delete acronyms[acronym];
-        bot.reply(message, `Ok, I won't explain '${acronym}' from now on.`);
+    if (acronyms[teamId].hasOwnProperty(acronym)) {
+        delete acronyms[teamId][acronym];
+        saveTeamAcronyms(teamId, () => bot.reply(message, `Ok, I won't explain '${acronym}' from now on.`));
     } else {
         console.log(`Unable to delete '${acronym}'. Acronym not found.`);
         bot.reply(message, 'Sorry, I don\'t know that acronym.');
@@ -76,17 +97,16 @@ controller.hears([
     /replace (?:\W)?([A-Za-z0-9\.]+)(?:\W)? with (?:\W)?(.+?)(?:\W)?$/
 ], directly, (bot, message) => {
 
+    const teamId = bot.team_info.id;
     const acronym = normaliseAcronym(message.match[1]);
     const expansion = entities.decodeHTML(message.match[2]);
 
-    if (acronyms.hasOwnProperty(acronym)) {
-        acronyms[acronym] = expansion;
-    } else {
-        console.log(`No existing acronym to update. Creating new acronym '${acronym}'.`);
+    if (!acronyms[teamId].hasOwnProperty(acronym)) {
+        console.log(`No existing acronym to update. Silently create new acronym '${acronym}'.`);
     }
 
-    controller.storage.teams.save({ id: message.team, acronyms });
-    bot.reply(message, 'Thanks for the update!');
+    acronyms[teamId][acronym] = expansion;
+    saveTeamAcronyms(teamId, () => bot.reply(message, 'Thanks for the update!'));
 });
 
 controller.hears([
@@ -95,11 +115,12 @@ controller.hears([
     /what does (.+) (?:mean|stand for)\b/
 ], directly, (bot, message) => {
 
+    const teamId = bot.team_info.id;
     const acronym = normaliseAcronym(message.match[1]);
     console.log(`User requested expansion of acronym: '${acronym}'`);
 
-    if (acronyms.hasOwnProperty(acronym)) {
-        bot.reply(message, `'${acronym}' stands for '${acronyms[acronym]}'`);
+    if (acronyms[teamId].hasOwnProperty(acronym)) {
+        bot.reply(message, `'${acronym}' stands for '${acronyms[teamId][acronym]}'`);
     } else {
         console.log(`Acronym '${acronym}' not found.`);
         bot.reply(message, 'Well, this is a bit embarrassing... I\'m afraid I don\'t know that one, but you can teach me!');
@@ -131,16 +152,17 @@ controller.hears([/(start|stop|are you) listening/], ['mention', 'direct_mention
 controller.on('ambient', (bot, message) => {
 
     if (listening.has(message.channel)) {
+        const teamId = bot.team_info.id;
         const tokenised = message.text.replace(/['",\.;:\?\(\)]/g, '').split(/\s/);
         const matched = tokenised.filter(part =>
             part.match(/^[A-Z]{2,}$/) &&
-            acronyms.hasOwnProperty(part) &&
+            acronyms[teamId].hasOwnProperty(part) &&
             !stopWords.includes(part));
 
         if (matched.length > 0) {
             const maybePlural = matched.length > 1 ? 's' : '';
             var response = `Acronym${maybePlural} detected!`;
-            matched.forEach(acronym => response += `\n'${acronym}' means '${acronyms[acronym]}'.`);
+            matched.forEach(acronym => response += `\n'${acronym}' means '${acronyms[teamId][acronym]}'.`);
             bot.reply(message, response);
         }
     }
